064ed64c73458996fcd9c686bdfa6acbeaf18f74
Victor <codingvictor22@gmail.com>
2022-01-20 21:47:34 +0100

    refactoring and flake8

--- Evenement/urls.py
+++ Evenement/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework_nested import routers
 
 from . import views
-from Evenement.admin import sales_site
+from Evenement.admin import epic_site
 
 router = routers.SimpleRouter()
 router.register(r'contrats', views.ContratViewSet, basename='contrats')
@@ -10,5 +10,5 @@ router.register(r'events', views.EventViewSet, basename='events')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('epiceventadmin/', sales_site.urls),
+    path('epiceventadmin/', epic_site.urls),
 ]
